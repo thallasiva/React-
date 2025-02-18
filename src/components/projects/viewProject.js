14266786a5aa1d4663ed147f04c627f33d42f716
Siva <thallasiva786@gmail.com>
2025-02-18 07:24:39 +0530

    view project ui and api intergrationc completed

--- src/components/projects/viewProject.js
+++ src/components/projects/viewProject.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react"
-import { useLocation } from "react-router-dom";
-
+import React, { useEffect, useState } from "react"
+import { data, useLocation } from "react-router-dom";
+import { Api_url, SELECTED_PROJECT_USERS } from "../../services/apiservice";
+import apiInstace from "../../interceptor/axiosInstance";
 const ViewProject = () => {
 
     const location = useLocation();
     const { project } = location.state || {};
+
+    const [getlocalStorage, setlocalStorage] = useState(null);
+    const [assingProjectMember, setAssginProjectMembers] = useState([])
 
     const tabsGroup = [
         { id: 1, name: "Edit Project" },
@@ -17,17 +21,52 @@ const ViewProject = () => {
 
 
     useEffect(() => {
-        console.log("project details here", project);
+        const otpResponse = JSON.parse(localStorage.getItem("otpValidateResponse"));
+        console.log("asfdsadfasf", otpResponse);
+
+        setlocalStorage(otpResponse.data.responseData[1][0]);
     }, [project]);
+
+    useEffect(() => {
+        if (getlocalStorage) {
+            projectMembers();
+        }
+    }, [getlocalStorage])
 
     const selectProjectButtonStyle = (selectedProject) => {
         console.log("Selected project details", selectedProject);
-        // You can add any logic for handling the selected button click here.
+
+        // You can add aprojny logic for handling the selected button click here.
+    }
+
+
+    // project member get api call
+
+    const projectMembers = () => {
+        console.log("get localstorage value", getlocalStorage);
+
+
+        let payload = {
+            projectId: project.projectId,
+            userId: getlocalStorage?.userId
+        };
+
+        apiInstace.post(Api_url + SELECTED_PROJECT_USERS, payload).then(resp => {
+            console.log("response", resp.data);
+
+            let names = resp.data.responseData[2][0].project_Users.filter((Object, index, listArr) => Object.status !== "No")
+            console.log(names);
+            setAssginProjectMembers(names)
+
+
+        }).catch(err => {
+            console.error("error occured", err);
+
+        })
     }
 
     return (
         <div>
-            <p> View Project application here</p>
             <div className="d-flex justify-content-start border border-primary p-3">
                 {
                     tabsGroup.map(item => {
@@ -37,12 +76,45 @@ const ViewProject = () => {
                         )
                     })
                 }
-                {/* <button className="ms-3 w-200" > Edit Project </button>
-                <button className="ms-3 w-200" > Files </button>
-                <button className="ms-3 w-200" > Milestone </button>
-                <button className="ms-3 w-200" > Posts </button>
-                <button className="ms-3 w-200" > Delete Project </button> */}
+
             </div>
+
+            <div className="form-group">
+                <label> Project Name</label>
+                <input type="text" placeholder="Project Name" className="form-control" value={project.projectName} readOnly />
+            </div>
+            <div className="form-group">
+                <label> Project Description</label>
+                <input type="text" placeholder="Project Description" className="form-control" value={project.projectDescription} readOnly />
+            </div>
+            <div className="form-group">
+                <label className="me-2">Project Members:</label>
+
+                <div className="d-flex border p-3">
+                    {assingProjectMember.map(item => (
+                        <span key={item.id} className="me-2 membersList">{item.userName}<br />{item.roleName}</span>
+                    ))}
+                </div>
+            </div>
+
+
+            <div className="row">
+                <div className="col form-group">
+                    <label> Start Date</label>
+                    <input type="date" className="form-control" value={project.startDate} readOnly />
+                </div>
+                <div className="col form-group">
+                    <label> End Date</label>
+                    <input type="date" className="form-control" value={project.endDate} readOnly />
+                </div>
+            </div>
+
+            <div className="form-group">
+                <label> Notes </label>
+                <textarea rows={5} placeholder="Note" className="form-control" value={project.notes} readOnly></textarea>
+            </div>
+
+
         </div >
     )
 
